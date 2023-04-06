af84d613577f0c3e8a97102e26c64385a3f9adeb
Geretik <59900066+Geretik@users.noreply.github.com>
2023-04-06 18:55:55 +0200

    Update Program.cs

--- StazEdhouse/Program.cs
+++ StazEdhouse/Program.cs
@@ -1,18 +1,44 @@
-﻿using System.Text;
+using System.Diagnostics;
+using System.Text;
 using StazEdhouse;
 
-//var contents = File.ReadAllText(@"/Users/gerete/Downloads/mapa.txt");
-
-//Console.WriteLine(contents.Length);
-
 var map = new StringBuilder("", 20000);
+var rows = 0;
+var cols = 0;
+var columns = new List<int>();
 
 while (Console.ReadLine() is { } line && line != "")
 {
+    foreach (var l in line)
+    {
+        switch (l)
+        {
+            case '\r':
+                continue;
+            case < '0' or > '9':
+                Console.WriteLine("Input error");
+                return;
+            default:
+                cols++;
+                break;
+        }
+    }
+
+    columns.Add(cols);
+    cols = 0;
+
+    rows++;
     map.Append(line);
     map.Append('\n');
 }
 
-var haf = new Map(map.ToString());
+cols = columns[0];
+if (columns.Any(i => i != cols))
+{
+    Console.WriteLine("Input error");
+    return;
+}
+
+var haf = new Map(map.ToString(), rows, columns[0]);
 
 Console.WriteLine(haf.GetVisibleCount());
